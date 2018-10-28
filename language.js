824ea15eff32464eb488a07da8e2656714581b39
Lennart Blom <registrierungen@lennart-blom.de>
2018-10-28 16:02:58 +0100

    Add translations and insert dynamic texts

--- language.js
+++ language.js
@@ -24,6 +24,32 @@ i18next
 function updateContent() {
     document.getElementById('tagline-text').innerHTML = i18next.t('text:tagline:text');
     document.getElementById('tagline-button').innerHTML = i18next.t('text:tagline:button');
+
+    document.getElementById('nav-about').innerHTML = i18next.t('text:navigation:about');
+    document.getElementById('nav-photography').innerHTML = i18next.t('text:navigation:photography');
+    document.getElementById('nav-platforms').innerHTML = i18next.t('text:navigation:platforms');
+    document.getElementById('nav-contact').innerHTML = i18next.t('text:navigation:contact');
+
+    document.getElementById('about-headline').innerHTML = i18next.t('text:about:headline');
+    document.getElementById('about-text').innerHTML = i18next.t('text:about:text');
+
+    document.getElementById('photography-headline').innerHTML = i18next.t('text:photography:headline');
+    document.getElementById('photography-text').innerHTML = i18next.t('text:photography:text');
+
+    document.getElementById('platforms-linkedin').innerHTML = i18next.t('text:platforms:linkedin');
+    document.getElementById('platforms-github').innerHTML = i18next.t('text:platforms:github');
+    document.getElementById('platforms-twitter').innerHTML = i18next.t('text:platforms:twitter');
+    document.getElementById('platforms-stackoverflow').innerHTML = i18next.t('text:platforms:stackoverflow');
+
+    document.getElementById('contact-slogan').innerHTML = i18next.t('text:contact:slogan');
+    document.getElementById('contact-contact').innerHTML = i18next.t('text:contact:contact');
+    document.getElementById('contact-instagram').innerHTML = i18next.t('text:contact:instagram');
+    document.getElementById('contact-thanks-headline').innerHTML = i18next.t('text:contact:thanks-headline');
+    document.getElementById('contact-thanks-text').innerHTML = i18next.t('text:contact:thanks-text');
+
+    document.getElementById('footer-imprint').innerHTML = i18next.t('text:footer:imprint');
+
+
 }
 
 function changeLng(lng) {
